b4ce8aefffe336e537435da2d5c45102972f9e95
mosu <peerakran03g@gmail.com>
2023-08-21 02:15:52 +0700

    Refactor:service register

--- service/register_db.php
+++ service/register_db.php
@@ -13,28 +13,28 @@
         
         if (empty($username)) {
         $_SESSION['error'] = 'please enter your username';
-        header("location: register.php");
+        header("location: ../Page/authentication/register.php");
     } else if (empty($password)) {
         $_SESSION['error'] = 'please enter your password';
-        header("location: register.php");
+        header("location: ../Page/authentication/register.php");
     } else if (str($_POST['password']) > 20 || strlen($_POST['password']) < 8 ) {
         $_SESSION['error'] = 'your password has been 8-20 word';
-        header("location: register.php");
+        header("location: ../Page/authentication/register.php");
     } else if (empty(c_$password)) {
         $_SESSION['error'] = 'please confirm your password';
-        header("location: register.php");
+        header("location: ../Page/authentication/register.php");
     } else if (empty($firstname)) {
         $_SESSION['error'] = 'please enter your firstname';
-        header("location: register.php");
+        header("location: ../Page/authentication/register.php");
     } else if (empty($lastname)) {
         $_SESSION['error'] = 'please enter your lastname';
-        header("location: register.php");
+        header("location: ../Page/authentication/register.php");
     } else if (empty($phonenumber)) {
         $_SESSION['error'] = 'please enter your phonenumber';
-        header("location: register.php");
+        header("location: ../Page/authentication/register.php");
     } else if ($password != $c_password){
         $_SESSION['error'] = 'your password is not match';
-        header("location: register.php");
+        header("location: ../Page/authentication/register.php");
     } else {
         try {
 
