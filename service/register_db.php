a39c6cd49ee0d360dff8cad1c5cb04b72a652a60
mosu <peerakran03g@gmail.com>
2023-08-25 00:33:14 +0700

    fix(bug): login&register system

--- service/register_db.php
+++ service/register_db.php
@@ -4,66 +4,64 @@
     require_once '../config/conn_db.php';
 
     if (isset($_POST['register'])) {
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
         $username = $_POST['username'];
         $password = $_POST['password'];
         $c_password = $_POST['c_password'];
-        $firstname = $_POST['firstname'];
-        $lastname = $_POST['lastname'];
         $phonenumber = $_POST['phonenumber'];
-        $user_id = $_POST['User_ID'];
+        
 
-        if (empty($username)) {
-            $_SESSION['error'] = 'please enter your username';
-            header("location: ../Page/authentication/register.php");
-        } else if (empty($password)) {
-            $_SESSION['error'] = 'please enter your password';
-            header("location: ../Page/authentication/register.php");
-        } else if (strlen($_POST['password']) > 20 || strlen($_POST['password']) < 8) {
-            $_SESSION['error'] = 'your password has been 8-20 word';
-            header("location: ../Page/authentication/register.php");
-        } else if (empty($c_password)) {
-            $_SESSION['error'] = 'please confirm your password';
-            header("location: ../Page/authentication/register.php");
-        } else if (empty($firstname)) {
-            $_SESSION['error'] = 'please enter your firstname';
-            header("location: ../Page/authentication/register.php");
+        if (empty($firstname)) {
+            $_SESSION['error'] = 'กรุณากรอกชื่อ';
+            header("location: ../../index.php");
         } else if (empty($lastname)) {
-            $_SESSION['error'] = 'please enter your lastname';
-            header("location: ../Page/authentication/register.php");
+            $_SESSION['error'] = 'กรุณากรอกนามสกุล';
+            header("location: ../../index.php");
+        } else if (empty($username)) {
+            $_SESSION['error'] = 'กรุณากรอกชื่อผู้ใช้';
+            header("location: ../../index.php");
         } else if (empty($phonenumber)) {
-            $_SESSION['error'] = 'please enter your phonenumber';
-            header("location: ../Page/authentication/register.php");
-        } else if ($password != $c_password){
-            $_SESSION['error'] = 'your password is not match';
-            header("location: ../Page/authentication/register.php");
+            $_SESSION['error'] = 'กรุณากรอกหมายเลขโทรศัพท์';
+            header("location: ../../index.php");
+        } else if (empty($password)) {
+            $_SESSION['error'] = 'กรุณากรอกรหัสผ่าน';
+            header("location: ../../index.php");
+        } else if (strlen($_POST['password']) > 20 || strlen($_POST['password']) < 5) {
+            $_SESSION['error'] = 'รหัสผ่านต้องมีความยาวระหว่าง 5 ถึง 20 ตัวอักษร';
+            header("location: ../../index.php");
+        } else if (empty($c_password)) {
+            $_SESSION['error'] = 'กรุณายืนยันรหัสผ่าน';
+            header("location: ../../index.php");
+        } else if ($password != $c_password) {
+            $_SESSION['error'] = 'รหัสผ่านไม่ตรงกัน';
+            header("location: ../../index.php");
         } else {
             try {
 
-                $check_email = $conn->prepare("SELECT User_ID FROM member WHERE User_ID = :User_ID");
-                $check_email->bindParam(":username", $username);
-                $check_email->execute();
-                $row = $check_email->fetch(PDO::FETCH_ASSOC);
+                $check_user = $conn->prepare("SELECT username FROM member WHERE username = :username");
+                $check_user->bindParam(":username", $username);
+                $check_user->execute();
+                $row = $check_user->fetch(PDO::FETCH_ASSOC);
 
                 if ($row['username'] == $username) {
-                    $_SESSION['warning'] = "มี username นี้อยู่ในระบบแล้ว <a href='../Page/authentication/login.php'>คลิ๊กที่นี่</a> เพื่อเข้าสู่ระบบ";
-                    header("../Page/authentication/login.php");
+                    $_SESSION['warning'] = "มีอีเมลนี้อยู่ในระบบแล้ว <a href='login.php'>คลิ๊กที่นี่</a> เพื่อเข้าสู่ระบบ";
+                    header("location: ../../index.php");
                 } else if (!isset($_SESSION['error'])) {
                     $passwordHash = password_hash($password, PASSWORD_DEFAULT);
-                    $stmt = $conn->prepare("INSERT INTO member(firstname, lastname, username, password, phonenumber, User_ID) 
-                                            VALUES(:firstname, :lastname, :username, :password, :phonenumber, :User_ID)");
+                    $stmt = $conn->prepare("INSERT INTO member(firstname, lastname, username, password, phonenumber) 
+                                            VALUES(:firstname, :lastname, :username, :password, :phonenumber)");
                     $stmt->bindParam(":firstname", $firstname);
                     $stmt->bindParam(":lastname", $lastname);
                     $stmt->bindParam(":username", $username);
-                    $stmt->bindParam(":phonenumber", $phonenumber);
                     $stmt->bindParam(":password", $passwordHash);
-                    $stmt->bindParam(":User_ID", $user_id);
-                    
+                    $stmt->bindParam(":phonenumber", $phonenumber);
                     $stmt->execute();
-                    $_SESSION['success'] = "สมัครสมาชิกเรียบร้อยแล้ว! <a href='../Page/authentication/login.php' class='alert-link'>คลิ๊กที่นี่</a> เพื่อเข้าสู่ระบบ";
-                    header("location: ../Page/authentication/login.php");
+                    $_SESSION['success'] = "สมัครสมาชิกเรียบร้อยแล้ว! <a href='login.php' class='alert-link'>คลิ๊กที่นี่</a> เพื่อเข้าสู่ระบบ";
+                    header("location: ../../index.php");
                 } else {
                     $_SESSION['error'] = "มีบางอย่างผิดพลาด";
-                    header("location: ../index.php");
+                    header("location: ../../index.php");
                 }
 
             } catch(PDOException $e) {
