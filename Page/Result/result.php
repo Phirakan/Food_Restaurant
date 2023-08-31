5c0f003e91172c04da550dac8c84f8604ad6d0cd
PhyschicWinter9 <suratlhaodee@gmail.com>
2023-09-01 02:06:08 +0700

    refactor(result) : improve design layout

--- Page/Result/result.php
+++ Page/Result/result.php
@@ -22,50 +22,36 @@ if (isset($_POST['submitOrder'])) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous" />
     <!-- CSS -->
     <link rel="stylesheet" href="../../css/index.css" />
     <link rel="stylesheet" href="../../css/edit-menu.css" />
+    <link rel="stylesheet" href="../../css/result.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" />
-    <style>       
-        .container {
-            margin-top: 20px;
-        }
-        
-        table {
-            width: 100%;
-            border-collapse: collapse;
-            border: 1px solid #ddd;
-        }
-        
-        th, td {
-            padding: 8px;
-            text-align: left;
-            border-bottom: 1px solid #ddd;
-        }
-        
-        th {
-            background-color: #f2f2f2;
-        }
-        
-        .text-end {
-            text-align: end;
-        }
+    <link rel="icon" href="../../assets/logo.png" type="image/x-icon" />
+    <title>สรุปคำสั่งซื้อ</title>
+    <style>
+
     </style>
 </head>
+
 <body>
     <!-- Your navigation bar and other UI elements here -->
-        <!-- Navbar -->
-        <nav class="navbar navbar-expand-md navbarcustom">
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-md navbarcustom">
         <div class="container-fluid">
             <a class="navbar-brand" href="../../index.php">
-                <img src="../../assets/logo.png" alt="Logo" width="50" height="50" class="d-inline-block align-text-top" />
+                <img src="../../assets/logo.png" alt="Logo" width="50" height="50"
+                    class="d-inline-block align-text-top" />
                 <span class="textbrand">อร่อยใกล้เคียง</span>
             </a>
 
             <!-- Add the hamburger menu button -->
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
@@ -73,7 +59,9 @@ if (isset($_POST['submitOrder'])) {
             <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                 <ul class="navbar-nav menunavbar">
                     <li class="nav-item username-layout">
-                        <p class="text-username">ร้าน <?php echo $_SESSION['username']; ?></p>
+                        <p class="text-username">ร้าน
+                            <?php echo $_SESSION['username']; ?>
+                        </p>
                     </li>
                     <li class="nav-item">
                         <a href="page/order-menu.php" class="btn btn-order-atnav">ออกจากระบบ</a>
@@ -84,75 +72,92 @@ if (isset($_POST['submitOrder'])) {
         </div>
     </nav>
     <div class="container">
-        <h3>อาหารที่สั่งของวันที่ <?php echo date("d/m/Y  H:i:s"); ?></h3>
+        <!-- <h3>อาหารที่สั่งของวันที่ <?php echo date("d/m/Y  H:i"); ?></h3>
        
-        <p>โต๊ะ: <?php echo isset($_GET['tableNumber']) ? $_GET['tableNumber'] : 'ไม่ระบุ'; ?></p>
-        
-        <h4>รายการอาหารที่สั่ง</h4>
-        <?php if (isset($_SESSION['cart']) && !empty($_SESSION['cart'])) : ?>
+        <p>โต๊ะ: <?php echo isset($_GET['tableNumber']) ? $_GET['tableNumber'] : 'ไม่ระบุ'; ?></p> -->
+
+        <h4>สรุปคำสั่งซื้อรายการอาหาร</h4>
+        <?php if (isset($_SESSION['cart']) && !empty($_SESSION['cart'])): ?>
             <table class="table">
                 <thead>
                     <tr>
-                        <th scope="col">ลำดับ</th>
                         <th scope="col">ชื่อเมนู</th>
                         <th scope="col">ราคา</th>
                         <th scope="col">จำนวน</th>
                         <th scope="col">ราคารวม</th>
-                        <th scope="col"></th>
+                        <th scope="col">โต๊ะ</th>
+                        <!-- <th scope="col"></th> -->
                     </tr>
                 </thead>
                 <tbody>
-    <?php
-    $totalPrice = 0;
-    $totalQuantity = 0; // Initialize total quantity
-    foreach ($_SESSION['cart'] as $index => $item) :
-        $itemTotal = $item['quantity'] * $item['price'];
-        $totalPrice += $itemTotal;
-        $totalQuantity += $item['quantity']; // Add to total quantity
-    ?>
-        <tr>
-            <th scope="row"><?php echo $index + 1; ?></th>
-            <td><?php echo $item['foodname']; ?></td>
-            <td><?php echo $item['price']; ?></td>
-            <td><?php echo $item['quantity']; ?></td>
-            <td><?php echo $itemTotal; ?></td>
-            <td>
+                    <?php
+                    $totalPrice = 0;
+                    $totalQuantity = 0; // Initialize total quantity
+                    foreach ($_SESSION['cart'] as $index => $item):
+                        $itemTotal = $item['quantity'] * $item['price'];
+                        $totalPrice += $itemTotal;
+                        $totalQuantity += $item['quantity']; // Add to total quantity
+                        ?>
+                        <tr>
+                            <!-- <th scope="row"><?php echo $index + 1; ?></th> -->
+                            <td>
+                                <?php echo $item['foodname']; ?>
+                            </td>
+                            <td>
+                                <?php echo $item['price']; ?>
+                            </td>
+                            <td>
+                                <?php echo $item['quantity']; ?>
+                            </td>
+                            <td>
+                                <?php echo $itemTotal; ?>
+                            </td>
+                            <td>
+                                <?php echo 1; ?>
+                            </td>
+                            <!-- <td>
                 <button class="btn btn-danger" onclick="removeItem('<?php echo $item['foodname']; ?>')">ลบ</button>
-            </td>
-            
-        </tr>
-    <?php endforeach; ?>
-    <tr>
-        <th colspan="3" class="text-end">รวมทั้งหมด</th>
-        <td><?php echo $totalQuantity; ?></td>
-        <td><?php echo $totalPrice; ?></td>
-    </tr>
-</tbody>
+            </td> -->
+
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
 
             </table>
-        <?php else : ?>
+        <?php else: ?>
             <p>ไม่มีรายการอาหาร</p>
         <?php endif; ?>
+        <!-- Display the total price -->
+        <div class="rol" style="margin-left: 10px; display:flex; gap: 8px;">
+            <p class="text-total-price">ทั้งหมด:
+                <?php echo $totalQuantity; ?> รายการ
+            </p>
+            <p class="text-total-price">ยอดรวมทั้งหมด:
+                <?php echo $totalPrice; ?> บาท
+            </p>
+        </div>
+
     </div>
     <!-- ... -->
 
     <script>
-    function removeItem(foodname) {
-        if (confirm('คุณต้องการลบรายการนี้ใช่หรือไม่?')) {
-            const xhr = new XMLHttpRequest();
+        function removeItem(foodname) {
+            if (confirm('คุณต้องการลบรายการนี้ใช่หรือไม่?')) {
+                const xhr = new XMLHttpRequest();
 
-            xhr.open('GET', `../../service/remove_item.php?foodname=${encodeURIComponent(foodname)}`, true);
+                xhr.open('GET', `../../service/remove_item.php?foodname=${encodeURIComponent(foodname)}`, true);
 
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-                    location.reload(); // Reload the page to reflect the updated cart
-                }
-            };
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                        location.reload(); // Reload the page to reflect the updated cart
+                    }
+                };
 
-            xhr.send();
+                xhr.send();
+            }
         }
-    }
-</script>
+    </script>
 
 </body>
+
 </html>
